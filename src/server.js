8deaae9238156bb7d1eb41a30ce4c4186971c715
Luiz Aureliano <luizaureliano.tds1.senai@gmail.com>
2024-11-21 16:52:57 -0300

    feat: adicionando comentarios no server.js

--- src/server.js
+++ src/server.js
@@ -1,17 +1,19 @@
-import express from "express";
-import { config } from "dotenv";
+import express from "express"; // importe do express para criar o servidor
+import routes from "../segunda_parte/routes/index.routes.js"; // import do arquivo routes
 
-config();
 
-const serverPort = process.env.PORT || 3000;
+import { config } from "dotenv";    //chamando tudo do .env
+config(); // carrega as varieáveis de ambiente .env
 
-const app = express();
-app.use(express.json());
+const app = express(); // criando uma instância do express
 
-app.get("/", (req, res) => {
-  return res.status(200).json({ message: "Hello, World!" });
-});
+const serverPort = process.env.PORT || 3000; // usando a porta padrão setado no arquivo .env ou 3000 como padrão
 
+app.use(express.json()); // configurar middleware para JSON, permitindo que o server entenda requisições em JSON
+
+app.use(routes); // configurar a middleware de rotas, permite que o server use as rotas do arquivo routes
+
+// iniciando o servidor
 app.listen(serverPort, () => {
-  console.log(`⚡ Server started on http://localhost:${serverPort}`);
+console.log(`⚡ Server started on http://localhost:${serverPort}`);
 });
