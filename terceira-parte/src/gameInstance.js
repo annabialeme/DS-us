f2d5e2ff886414ca935ffc9e068b4e8711f3fa5c
anna leme <annaleme.tds1.senai@gmail.com>
2024-11-28 10:44:33 -0300

    feat: gameInstance

---
+++ terceira-parte/src/gameInstance.js
@@ -0,0 +1,24 @@
+import Jogo from "./models/Jogo.js";
+
+const gameInstance = new Jogo();
+
+gameInstance.lugares = [
+"Recepção",
+"Banheiro",
+"Corredor",
+"Sala de aula",
+"Refeitório",
+"Oficina",
+"Secretaria",
+"Laboratório",
+"Quadra",
+"Empilhadeira",
+"Diretoria",
+"Biblioteca",
+"Analista de qualidade de vida",
+"Auditório",
+"Ponto de encontro",
+"Estacionamento"
+]
+
+export default gameInstance;
