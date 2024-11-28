e6566093061b38b8a495fe99c0b944dbdc8ba4e4
Anna Beatriz <annavalentim.tds1.senai@gmail.com>
2024-11-28 11:23:11 -0300

    rota delete realizada

--- terceira-parte/src/routes/cadastro.routes.js
+++ terceira-parte/src/routes/cadastro.routes.js
@@ -32,9 +32,26 @@ cadastroRoutes.put("/atualizar", (req, res) => {
 
 });
 
+//Rota para deletar um usuario
 cadastroRoutes.delete("/remover", (req, res) => {
+    const { id } = req.params;
 
+//Busca um usuario pelo id no array de usuarios
+    const usuario = usuario.find((usuario) => usuario.id == id);
+
+    if (!usuario) {
+        return res.status(404).json({ message: `Usuario com id ${id} não encontrado!` });
+}
+
+//Remove o usuario do array de usuarios
+usuario = usuario.filter((usuario) => usuario.id != id);
+
+return res.status(200).json({ message: "Usuário removido com sucesso!",
+    usuario,
+ });
 });
+
+
 });
 
 export default cadastroRoutes;
