bac136ba5de95827c5337924d850d91e4ef8a10d
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2024-12-05 16:28:17 -0300

    feat: adicionando comentário

--- primeira-parte/quiz.js
+++ primeira-parte/quiz.js
@@ -1,16 +1,20 @@
+// Classe que representa um Quiz
 class Quiz {
     constructor() {
     this.perguntas = [];
     }
 
+ // Método para adicionar perguntas ao quiz
     obterPerguntaAleatoria() {
       const indiceAleatorio = Math.floor(Math.random() * this.perguntas.length);
     return this.perguntas[indiceAleatorio];
     }
 
+    // Método para adicionar perguntas ao quiz
     verificarResposta(pergunta, respostaDada) {
     return pergunta.respostaCorreta === respostaDada;
     }
 }
 
+// Classe que representa uma pergunta
 export default Quiz;
