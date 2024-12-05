52ee9306ac40894fe32bcd52fc535f913ccc8f8d
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2024-12-05 16:44:26 -0300

    feat: código oficialmente comentado!

--- primeira-parte/quiz.js
+++ primeira-parte/quiz.js
@@ -1,20 +1,26 @@
-// Classe que representa um Quiz
+// Define a classe Quiz, que encapsula funcionalidades relacionadas a um questionário
 class Quiz {
+// O construtor é chamado ao criar uma nova instância da classe Quiz
     constructor() {
+// Inicializa a propriedade perguntas como um array vazio para armazenar as perguntas do quiz
     this.perguntas = [];
     }
 
- // Método para adicionar perguntas ao quiz
+// Método para obter uma pergunta aleatória do array de perguntas
     obterPerguntaAleatoria() {
+
+// Calcula um índice aleatório baseado no tamanho do array de perguntas
       const indiceAleatorio = Math.floor(Math.random() * this.perguntas.length);
+// Retorna a pergunta correspondente ao índice aleatório gerado
     return this.perguntas[indiceAleatorio];
     }
 
-    // Método para adicionar perguntas ao quiz
+// Método para verificar se a resposta dada está correta
     verificarResposta(pergunta, respostaDada) {
+         // Compara a resposta correta da pergunta com a resposta fornecida e retorna true ou false
     return pergunta.respostaCorreta === respostaDada;
     }
 }
 
-// Classe que representa uma pergunta
+// Exporta a classe Quiz como padrão, permitindo sua importação em outros módulos
 export default Quiz;
